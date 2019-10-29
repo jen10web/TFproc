5acfa2ff82113ee4ab75e07f76b503579e2586eb
Jenna Webster <jen10web@gmail.com>
2019-10-29 10:10:10 -0600

    initial check in

---
+++ ProcConnect.v
@@ -0,0 +1,14 @@
+module ProcConnect(clk, reset, loadedAdr, dataFromMem);
+input clk, reset;
+input [15:0] loadedAdr;
+output [15:0] dataFromMem;
+
+wire [15:0] adrToMem, dataFromMem, dataToMem;
+wire memWrite, memRead;
+
+Processor proc(clk, reset, dataFromMem, loadedAdr, memRead, memWrite, adrToMem, dataToMem);
+
+memTest memory(~clk, 1'b1, memWrite, adrToMem, dataToMem, dataFromMem );
+
+endmodule
+
